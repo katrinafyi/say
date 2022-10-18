163b21a9e84101faf26a6054cc047b6487f11422
Kait Lam <k@rina.fyi>
2022-10-18 22:55:08 +1000

    trans T_T

--- src/Memo.hs
+++ src/Memo.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Memo where
 
@@ -6,6 +7,9 @@ import           Control.Monad.Trans.State.Strict
 import           Data.Function                  ( fix )
 import           Data.Functor.Identity          ( Identity(runIdentity) )
 
+import           Control.Applicative            ( Applicative(liftA2) )
+import           Control.Monad.Trans.Class
+import           Control.Monad.Trans.Identity   ( runIdentityT )
 import           Data.Map                       ( Map )
 import qualified Data.Map.Strict               as Map
 
@@ -15,26 +19,48 @@ type OpenFun a b = (a -> b) -> a -> b
 fib :: (Integer -> Integer) -> Integer -> Integer
 fib rec n = if n <= 1 then 1 else rec (n - 1) + rec (n - 2)
 
-fib2 :: Monad m => (Integer -> m Integer) -> Integer -> m Integer
-fib2 rec n = if n <= 1 then pure 1 else (+) <$> rec (n - 1) <*> rec (n - 2)
+type Memoisable m a b
+  = forall t . (MonadTrans t, Monad (t m)) => OpenFun a (t m b)
 
-type Memoisable a b = forall m . Monad m => OpenFun a (m b)
+fib2 :: Memoisable Identity Integer Integer
+fib2 rec n =
+  if n <= 1 then lift $ pure 1 else liftA2 (+) (rec (n - 1)) (rec (n - 2))
 
-naive :: Memoisable a b -> a -> b
-naive f = runIdentity . fix f
+fib3
+  :: (MonadTrans t, Monad (t IO))
+  => (Integer -> t IO Integer)
+  -> Integer
+  -> t IO Integer
+fib3 rec n = lift (print n)
+  >> if n <= 1 then lift $ pure 1 else liftA2 (+) (rec (n - 1)) (rec (n - 2))
 
-type Memoised a b = Ord a => a -> State (Map a b) b
 
-nomemoise :: Memoisable a b -> Memoised a b
-nomemoise f = pure . naive f
+type Memoised m a b = a -> StateT (Map a b) m b
 
-memoise :: Memoisable a b -> Memoised a b
-memoise f a = gets (Map.lookup a) >>= maybe compute pure
+naive :: (forall m . Memoisable m a b) -> a -> b
+naive f = runIdentity . runIdentityT . fix f
+
+nomemoise :: (Monad m) => Memoisable m a b -> Memoised m a b
+nomemoise f = fix f
+
+memoise :: (Monad m, Ord a) => Memoisable m a b -> Memoised m a b
+memoise f a = do
+  old <- gets (Map.lookup a)
+  maybe compute pure old
  where
   compute = do
     x <- f (memoise f) a
     modify (Map.insert a x)
     pure x
+
+nomemoExample :: [Integer]
+nomemoExample =
+  runIdentity $ evalStateT (traverse (nomemoise fib2) [1 .. 30]) Map.empty
+
+memoExample :: [Integer]
+memoExample =
+  runIdentity $ evalStateT (traverse (memoise fib2) [1 .. 30]) Map.empty
+
 
 {-
 example output:
